71e99f76c3239b9d2c21cab28d33618880e45006
Ben Graves <bengraves@Bens-MacBook-Pro.local>
2019-11-01 17:19:32 +0000

    reconfigure white noise

--- interviewtool/interviewtool/settings.py
+++ interviewtool/interviewtool/settings.py
@@ -129,4 +129,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROUTE = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
