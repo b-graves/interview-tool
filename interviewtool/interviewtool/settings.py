ae0117d89432ce74951903e59005442f6cf830b7
Ben Graves <bengraves@Bens-MacBook-Pro.local>
2019-11-01 17:24:28 +0000

    add static route

--- interviewtool/interviewtool/settings.py
+++ interviewtool/interviewtool/settings.py
@@ -130,3 +130,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROUTE = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
